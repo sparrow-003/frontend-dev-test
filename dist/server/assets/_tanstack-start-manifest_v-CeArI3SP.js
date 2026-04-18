24df3d327146112ab7a6c8698bd7e592ca0ed28f
nagarajan <nagarajan1980s@gmail.com>
2026-04-18 16:58:36 +0530

    Add production build dist

---
+++ dist/server/assets/_tanstack-start-manifest_v-CeArI3SP.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/frontend-dev-test/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-FFFWii3N.js"] }, "/": { filePath: "D:/frontend-dev-test/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BHFajjxU.js"] } }, clientEntry: "/assets/index-FFFWii3N.js" });
+export {
+  tsrStartManifest
+};
